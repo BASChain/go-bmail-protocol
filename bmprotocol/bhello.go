da09bc2e1b24255a27eed519ae3ed033fdbe9535
stephenliao-tutan <stephen.liao@tutanota.com>
2020-04-25 00:00:05 +0800

    add to repo

---
+++ bmprotocol/bhello.go
@@ -0,0 +1,234 @@
+package bmprotocol
+
+import (
+	"github.com/BASChain/go-bmail-protocol/translayer"
+	"encoding/binary"
+	"github.com/pkg/errors"
+	"github.com/kprc/nbsnetwork/tools"
+)
+
+type BMHello struct {
+
+}
+
+func NewBMHello() *BMHello {
+	return &BMHello{}
+}
+
+func (bmh *BMHello)Pack() ([]byte, error) {
+	bmact:= translayer.NewBMTL(HELLO,nil)
+
+	return bmact.Pack()
+}
+
+func (bmh *BMHello)UnPack(data []byte) (int,error) {
+	//nothing todo
+	return 0,nil
+}
+
+
+type BMHelloACK struct {
+	sn []byte
+}
+
+func NewBMHelloACK(sn []byte) *BMHelloACK {
+	return &BMHelloACK{}
+}
+
+
+
+func (bmha *BMHelloACK)Pack() ([]byte,error)  {
+	var barr []byte
+
+	bufl:=translayer.UInt16ToBuf(uint16(len(bmha.sn)))
+
+	barr = append(barr,bufl...)
+	barr = append(barr,bmha.sn...)
+
+	bmact:= translayer.NewBMTL(HELLO_ACK,barr)
+
+	return bmact.Pack()
+}
+
+func (bmha *BMHelloACK)UnPack(data []byte) (int,error) {
+
+	if len(data)<2{
+		return 0,errors.New("Not a HELLO ACK data")
+	}
+
+	l:=binary.BigEndian.Uint16(data)
+
+	bmha.sn = data[2:]
+
+	if l != uint16(len(bmha.sn)){
+		return 0,errors.New("Serial Nunber Error")
+	}
+
+	return 2+len(bmha.sn),nil
+}
+
+
+/*
+*
+
+pack:
+1.NewSendSignature
+2.ForSigBuf
+3.Calculate signature
+4.SetSig
+5.Pack
+6.Send
+
+unpack:
+bmtl:=&BMTransLayer{}
+bmtl.UnPack(buf)
+if bmtl.typ == SEND_SIGNATURE{
+	ss:=&SendSignature{}
+	ss.UnPack(buf[bmtl.GetData()])
+}
+
+*/
+
+type SendSignature struct {
+	sn []byte
+	localMailAddr string
+	currentTime int64      //Millisecond
+	sig []byte
+}
+
+func NewSendSignature(sn []byte,localMailAddr string) *SendSignature {
+	ss := &SendSignature{}
+	ss.sn = sn
+	ss.localMailAddr = localMailAddr
+	ss.currentTime = tools.GetNowMsTime()
+
+	return ss
+}
+
+
+
+func (ss *SendSignature)ForSigBuf() []byte  {
+	var r []byte
+
+	bufl:=translayer.UInt16ToBuf(uint16(len(ss.sn)))
+
+	r = append(r,bufl...)
+
+	r = append(r,ss.sn...)
+
+	bufl = translayer.UInt16ToBuf(uint16(len(ss.localMailAddr)))
+
+	r = append(r,bufl...)
+
+	r = append(r,[]byte(ss.localMailAddr)...)
+
+	bufl = translayer.UInt64ToBuf(uint64(ss.currentTime))
+
+	r = append(r,bufl...)
+
+	return r
+}
+
+func (ss *SendSignature)SetSig(sig []byte)  {
+	ss.sig = sig
+}
+
+func (ss *SendSignature)Pack() ([]byte,error)  {
+	r:=ss.ForSigBuf()
+
+	bufl:=translayer.UInt16ToBuf(uint16(len(ss.sig)))
+
+	r = append(r,bufl...)
+
+	r = append(r,ss.sig...)
+
+	bmact:= translayer.NewBMTL(SEND_SIGNATURE,r)
+
+	return bmact.Pack()
+}
+
+func (ss *SendSignature)UnPack(buf []byte)  (int,error) {
+	offset:=0
+
+	if len(buf[offset:])<2{
+		return 0,errors.New("Not a SendSignature data")
+	}
+
+	lsn:=binary.BigEndian.Uint16(buf[offset:])
+	offset += 2
+	if len(buf)<offset+int(lsn){
+		return 0,errors.New("Not a SendSignature data")
+	}
+	ss.sn = buf[offset:offset+int(lsn)]
+	offset += int(lsn)
+
+	if len(buf[offset:])<2{
+		return 0,errors.New("Not a SendSignature data")
+	}
+	laddr:=binary.BigEndian.Uint16(buf[offset:])
+	offset += 2
+	if len(buf)<offset+int(laddr){
+		return 0,errors.New("Not a SendSignature data")
+	}
+	ss.localMailAddr = string(buf[offset:offset+int(laddr)])
+	offset += int(laddr)
+	if len(buf[offset:])<8{
+		return 0,errors.New("Not a SendSignature data")
+	}
+	ss.currentTime = int64(binary.BigEndian.Uint64(buf[offset:]))
+	offset += 8
+
+	if len(buf[offset:])<2{
+		return 0,errors.New("Not a SendSignature data")
+	}
+
+	lsig:=binary.BigEndian.Uint16(buf[offset:])
+	offset += 2
+	if len(buf)<offset + int(lsig){
+		return 0,errors.New("Not a SendSignature data")
+	}
+
+	ss.sig = buf[offset:]
+
+	return offset + int(lsig),nil
+
+}
+
+type ValidateSignature struct {
+	sn []byte
+}
+
+
+func NewValidSign(sn []byte) *ValidateSignature {
+	return &ValidateSignature{}
+}
+
+func (vs *ValidateSignature)Pack() ([]byte,error)  {
+	var barr []byte
+
+	bufl:=translayer.UInt16ToBuf(uint16(len(vs.sn)))
+
+	barr = append(barr,bufl...)
+	barr = append(barr,vs.sn...)
+
+	bmact:= translayer.NewBMTL(VALIDATE_SIGNATURE,barr)
+
+	return bmact.Pack()
+}
+
+func (vs *ValidateSignature)UnPack(data []byte) (int,error) {
+
+	if len(data)<2{
+		return 0,errors.New("Not a Validate Signature data")
+	}
+
+	l:=binary.BigEndian.Uint16(data)
+
+	vs.sn = data[2:]
+
+	if l != uint16(len(vs.sn)){
+		return 0,errors.New("Serial Nunber Error")
+	}
+
+	return 2+len(vs.sn),nil
+}
---
+++ translayer/bmactiontype.go
@@ -0,0 +1,122 @@
+package translayer
+
+import (
+	"github.com/BASChain/go-bmail-protocol/bmprotocol"
+	"github.com/pkg/errors"
+	"encoding/binary"
+)
+
+const BMAILVER1 uint16 = 1
+const ED25519 uint16 = 1
+
+type BMTransLayer struct {
+	ver uint16
+	cryptType uint16
+	typ uint16
+	data []byte
+}
+
+func (bmtl *BMTransLayer)GetData() []byte  {
+	return bmtl.data
+}
+
+func NewBMTL(typ uint16,data []byte) *BMTransLayer {
+	bmtl:=&BMTransLayer{}
+
+	bmtl.ver = BMAILVER1
+	bmtl.cryptType = ED25519
+
+	bmtl.typ = typ
+	bmtl.data = data
+
+	return bmtl
+}
+
+func UInt16ToBuf(ui16 uint16) []byte {
+	bufl:=make([]byte,2)
+
+	binary.BigEndian.PutUint16(bufl,ui16)
+
+	return bufl
+}
+
+func UInt32ToBuf(ui32 uint32) []byte {
+	bufl:=make([]byte,4)
+
+	binary.BigEndian.PutUint32(bufl,ui32)
+
+	return bufl
+}
+
+func UInt64ToBuf(ui64 uint64 ) []byte {
+	bufl:=make([]byte,8)
+
+	binary.BigEndian.PutUint64(bufl,ui64)
+
+	return bufl
+}
+
+
+
+func (bmtl *BMTransLayer)Pack() ([]byte,error) {
+
+	if bmtl.typ <= bmprotocol.MIN_TYP || bmtl.typ>bmprotocol.MAX_TYP{
+		return nil,errors.New("BMail Action Type Error")
+	}
+	var r []byte
+
+	bufl:=UInt16ToBuf(uint16(bmtl.ver))
+	r = append(r,bufl...)
+
+	bufl=UInt16ToBuf(uint16(bmtl.cryptType))
+	r = append(r,bufl...)
+
+	bufl=UInt16ToBuf(uint16(bmtl.typ))
+	r = append(r,bufl...)
+
+	if len(bmtl.data)>0{
+		l:=uint32(len(bmtl.data))
+
+
+		bufl := UInt32ToBuf(uint32(l))
+
+		r = append(r,bufl...)
+
+		r = append(r,bmtl.data...)
+	}
+
+	return r,nil
+}
+
+func (bmtl *BMTransLayer)UnPack(data []byte) (int,error) {
+
+	if len(data)<10{
+		return 0,errors.New("Not a BMail Action Data")
+	}
+
+	offset := 0
+	bmtl.ver = binary.BigEndian.Uint16(data[offset:])
+	offset += 2
+
+	bmtl.cryptType = binary.BigEndian.Uint16(data[offset:])
+	offset += 2
+
+	bmtl.typ = binary.BigEndian.Uint16(data[offset:])
+	offset += 2
+
+	if bmtl.typ <= bmprotocol.MIN_TYP || bmtl.typ>=bmprotocol.MAX_TYP{
+		return 0,errors.New("BMail Action Type Error")
+	}
+
+	l:=binary.BigEndian.Uint32(data[offset:])
+	offset += 4
+
+	bmtl.data = data[offset:]
+
+	if l != uint32(len(bmtl.data)){
+		return 0,errors.New("Data Length Error")
+	}
+
+	return offset + len(bmtl.data),nil
+}
+
