89b24b493f28d34682a2fd89998c75c1f4afc016
stephenliao-tutan <stephen.liao@tutanota.com>
2020-04-26 09:30:17 +0800

    add to repo

--- bmprotocol/bmtp.go
+++ bmprotocol/bmtp.go
@@ -1,1 +1,40 @@
 package bmprotocol
+
+import "github.com/BASChain/go-bmail-protocol/translayer"
+
+
+//client -> server
+type SendEnvelope struct {
+	translayer.BMTransLayer
+	From string
+	RecpAddr string     //recipient
+	LAddr []byte		//local public key
+	//===cipher text begin====
+	To []string
+	CC []string
+	BC []string
+	Subject string
+	Data string
+	//====cipher text end ===
+	IV []byte			//sn from Bhello
+	Sig []byte          //signature
+}
+
+
+//server -> client
+type RespSendEnvelope struct {
+	translayer.BMTransLayer
+	IV []byte			//same as SendEnvelope
+}
+
+
+//server -> client
+type SendEnvelopeFail struct {
+
+}
+
+
+//client -> server
+type RespSendEnvelopeFail struct {
+	
+}
